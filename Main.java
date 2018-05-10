e1d2b79b3c5c520144e8089caf19c294be3a7804
Jacobo Diego Pita Hernandez <34080303+JacoboDPH@users.noreply.github.com>
2018-05-10 16:24:56 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+		System.out.println ("Bienvenido al sistema");
+		System.out.println ("Hoy es lunes");
+		System.out.println ("Son las 08:00 horas");
+		System.out.println ("Tiene 8 correos sin leer");
+		
+		
+	}
+
+}
